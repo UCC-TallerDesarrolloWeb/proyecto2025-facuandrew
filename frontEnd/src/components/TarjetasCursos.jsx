2ac932c9c16d5bd21c55baaf5bf9e968665deebd
facucarattoliUCC <2418100@ucc.edu.ar>
2025-11-09 11:54:46 -0300

    img y estilos para tarjetas cursos

--- frontEnd/src/components/TarjetasCursos.jsx
+++ frontEnd/src/components/TarjetasCursos.jsx
@@ -1,4 +1,9 @@
 import "../styles/TarjetasCursos.css"
+import mountainBikeImg from "../assets/imagenes/mountainBike.jpeg"
+import cursoIntermedioImg from "../assets/imagenes/cursoIntermedio.jpg"
+import escaladaImg from  "../assets/imagenes/escalada.jpeg"
+import cursoAvanzadaImg from "../assets/imagenes/cursoAvanzado.jpg"
+import trekkingImg from "../assets/imagenes/trekking.jpeg"
 
 const tarjetasCursos = () => {
     const cursos = [
@@ -9,7 +14,7 @@ const tarjetasCursos = () => {
             detalleCurso: "Objetivos del curso: Que los alumnos obtengan todos los conocimientos teóricos y prácticos para poder ejecutar escaladas en montañas de roca vertical, mediante la progresión en libre y aseguramiento con empotradores",
             categoria: "escalada",
             precio: 65000,
-            imagen: "cursoIntermedio.jpg",
+            imagen: cursoIntermedioImg,
             horario: [
                 { dia: 5, "inicio":"8:00", "fin":"17:00"}
             ]
@@ -19,9 +24,9 @@ const tarjetasCursos = () => {
             nombre: "Curso Intermedio Escalada en Roca",
             descripcion: "Escala al siguiente nivel. Requerido conocimientos previos. Para mas detalle presione ver detalle",
             detalleCurso: "Curso intermedio de escalada en roca Nivel 2. Se enfoca en progresiones de varios largos, técnicas de escalada en punta, anclajes, rapel avanzado y colocación de material, para que puedas escalar como primero de cordada en vías equipadas y semiequipadas. Para poder realizarlo es necesario poder escalar vía de grado V+ y saber montar reuniones y descolgarse con seguridad. Si no es así, deberán realizar el curso de nivel 1",
-            categoria: "escalada",
+            categoria: escaladaImg,
             precio: 100000,
-            imagen: "escalada.jpeg",
+            imagen: cursoIntermedioImg,
             horario: [
                 { dia: 2, "inicio":"8:00", "fin":"17:00"},
                 {dia: 3, "inicio":"8:00", "fin":"17:00"}
@@ -34,7 +39,7 @@ const tarjetasCursos = () => {
             detalleCurso: "Curso Avanzado Escalada en Roca nivel 3 requiere superar una prueba de ingreso. Es necesario tener experiencia para avanzar en situaciones y terrenos complejos.",
             categoria: "escalada",
             precio: 200000,
-            imagen: "cursoAvanzado.jpg",
+            imagen: cursoAvanzadaImg,
             horario: [
                 { dia: 1, "inicio":"8:00", "fin":"19:00"},
                 {dia: 0, "inicio":"8:00", "fin":"19:00"}
@@ -47,7 +52,7 @@ const tarjetasCursos = () => {
             detalleCurso: "Se realizan salidas grupales a distintos lugares. Dependiendo el lugar y las horas de caminata se provee alimento y bebida. El transporte hacia el lugar es independiente de cada participante. Consultar por disponibilidad y proximas salidas",
             categoria: "trekking",
             precio: 20000,
-            imagen: "trekking.jpeg",
+            imagen: trekkingImg,
             horario: [
                 { dia: 5, "inicio":"7:00", "fin":"18:00"},
                 {dia: 4, "inicio":"8:00", "fin":"20:00"}
@@ -60,7 +65,7 @@ const tarjetasCursos = () => {
             detalleCurso: "Se eligen diferentes rutas en cada salida. El transporte hasta el lugar es independiente de cada persona. Nosotros nos encargamos de todo, bici, repuestos, elementos de seguridad, alimentos, bebida,",
             categoria: "mountain bike",
             precio: 120000,
-            imagen: "mountain%20bike.jpeg",
+            imagen: mountainBikeImg,
             horario: [
                 { dia: 6, "inicio":"9:00", "fin":"20:00"},
                 {dia: 5, "inicio":"8:00", "fin":"17:00"}
@@ -83,6 +88,7 @@ const tarjetasCursos = () => {
             {cursos.map((curso, indice) => (
                 <div className="tarjeta-curso" key={indice}>
                     <h3>{curso.nombre}</h3>
+                    <img className="contenedor-img-cursos" src={curso.imagen} alt={curso.nombre}/>
                     <p>{curso.descripcion}</p>
                     <ul>
                         {/*ind = indice identificador,  */}
